b8735f0bbb2e9062d4fc1dd0f34691ef98498c3b
FellipeF <fellipefreire99@hotmail.com>
2024-07-04 19:36:25 -0300

    Solução ruim para tratamento de exceções

---
+++ src/modelo/entidades/Reserva.java
@@ -0,0 +1,58 @@
+package modelo.entidades;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reserva {
+    
+    private Integer numeroQuarto;
+    private Date checkin;
+    private Date checkout;
+    
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    public Reserva() {
+    }
+
+    public Reserva(Integer numeroQuarto, Date checkin, Date checkout) {
+        this.numeroQuarto = numeroQuarto;
+        this.checkin = checkin;
+        this.checkout = checkout;
+    }
+
+    public Integer getNumeroQuarto() {
+        return numeroQuarto;
+    }
+
+    public void setNumeroQuarto(Integer numeroQuarto) {
+        this.numeroQuarto = numeroQuarto;
+    }
+
+    public Date getCheckin() {
+        return checkin;
+    }
+
+    public Date getCheckout() {
+        return checkout;
+    }
+    
+    public long duracao()
+    {
+        long dif = checkout.getTime() - checkin.getTime();
+        return TimeUnit.DAYS.convert(dif, TimeUnit.MILLISECONDS);
+    }
+    
+    public void atualizarDatas(Date checkin, Date checkout)
+    {
+        this.checkin = checkin;
+        this.checkout = checkout;
+    }
+    
+    @Override
+    public String toString()
+    {
+        return "Sala " + numeroQuarto + ", check-in: " + sdf.format(checkin) + ", check-out: " + sdf.format(checkout) + ", " + duracao() + " noites";
+    }
+    
+}
